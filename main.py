136f66f2759a2c711554965c627963895d71db14
emp0ry <64217088+emp0ry@users.noreply.github.com>
2023-04-15 13:21:51 +0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ except: os.system("pip install pynput"); from pynput import keyboard as kb
 try: import multiprocessing as mp
 except:  os.system("pip install multiprocess"); import multiprocessing as mp
 
-BIND = kb.Key.f1 # f1
+BIND1 = kb.Key.f4 # for turn on or turn off script
+BIND2 = kb.Key.f3 # for change angle of antiaim
 
 # detect if you in csgo
 def in_csgo():
@@ -27,38 +28,62 @@ def in_csgo():
     except:
         in_csgo()
 
-def on_press(key, event):
+def on_press(key, event1, event2):
     try:
         global timer
-        if in_csgo() and key == BIND and time.time() - timer > 1.5:
-            if event.is_set():
-                winsound.Beep(600, 250)
-                event.clear()
-            else:
-                winsound.Beep(800, 250)
-                event.set()
-            timer = time.time()
+        if in_csgo() and time.time() - timer > 1:
+            if key == BIND1:
+                if event1.is_set():
+                    winsound.Beep(600, 250)
+                    print('OFF')
+                    event1.clear()
+                else:
+                    winsound.Beep(800, 250)
+                    print('ON')
+                    event1.set()
+                timer = time.time()
+            elif key == BIND2:
+                if event2.is_set():
+                    winsound.Beep(400, 100)
+                    print('left angle')
+                    event2.clear()
+                else:
+                    winsound.Beep(400, 100)
+                    print('right angle')
+                    event2.set()
+                timer = time.time()
+            
+        
     except AttributeError:
         pass
 
 # antiaim
-def when_pressed(event):
+def when_pressed(event1, event2):
     while True:
-        if event.is_set() and in_csgo():
-            time.sleep(150/1000)
-            ctypes.windll.user32.mouse_event(0x0001, 4500, 1500)
-            time.sleep(10/1000)
-            ctypes.windll.user32.mouse_event(0x0001, -4500, -1500)
+        if event1.is_set() and in_csgo():
+            if event2.is_set(): # right antiaim
+                time.sleep(150/1000)
+                ctypes.windll.user32.mouse_event(0x0001, 4500, 1500)
+                time.sleep(10/1000)
+                ctypes.windll.user32.mouse_event(0x0001, -4500, -1500)
+            else: # left antiaim
+                time.sleep(150/1000)
+                ctypes.windll.user32.mouse_event(0x0001, -4500, -1500)
+                time.sleep(10/1000)
+                ctypes.windll.user32.mouse_event(0x0001, 4500, 1500)
+
         else:
             time.sleep(0.05)
 
 if __name__ == '__main__':
     timer = 0
     with mp.Manager() as manager:
-        event = manager.Event()
-        process = mp.Process(target=when_pressed, args=(event,))
-        with kb.Listener(on_press=lambda key: on_press(key, event)) as listener:
+        event1 = manager.Event()
+        event2 = manager.Event()
+        process = mp.Process(target=when_pressed, args=(event1, event2))
+        with kb.Listener(on_press=lambda key: on_press(key, event1, event2)) as listener:
             process.start()
             listener.join()
-            event.clear()  # Stop the loop
+            event1.clear()
+            event2.clear()
             process.terminate()
